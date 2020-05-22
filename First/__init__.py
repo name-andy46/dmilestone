32fc0bb70a67edf71c059c5b05e8ad715893089c
Anant Dongre <anant.dongre46@gmail.com>
2020-05-22 07:24:33 +0000

    third commit- updated with environment variable. Making public

--- First/__init__.py
+++ First/__init__.py
@@ -14,7 +14,8 @@ csrf.init_app(app)
 
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///dharamshi.db'
-app.config['SECRET_KEY'] = 'a67101b61d10691bf125f766b0249ea5'
+app.config['SECRET_KEY'] = os.getenv("secret")
+
 
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
@@ -26,8 +27,8 @@ app.config['MAIL_SERVER'] = 'smtp.gmail.com'
 app.config['MAIL_PORT'] = 465
 app.config['MAIL_USE_TLS'] = False
 app.config['MAIL_USE_SSL'] = True
-app.config['MAIL_USERNAME'] = 'website.info1.9047@gmail.com'
-app.config['MAIL_PASSWORD'] = 'N0wyv#266iD!GwV%'
+app.config['MAIL_USERNAME'] = os.getenv("UserN")
+app.config['MAIL_PASSWORD'] = os.getenv("PassW")
 mail = Mail(app)
 
 
